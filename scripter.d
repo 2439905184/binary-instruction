aef8a3e1a705cefd702437648ba819458b0cc21a
2439905184 <suchenglong018@126.com>
2022-07-01 20:39:13 +0800

    ✨ feat: 初步完成对二进制指令的读取

--- scripter.d
+++ scripter.d
@@ -7,11 +7,20 @@ void main(string[] args)
     File file = File(args[1],"r");
     //writeln(args[1]);
     //先读取两个字节 这里的编译器没做好
-    void[] a = read(file_name,1);
-    writeln(a[0]);
-    if(a[0] == 1)
+    byte[] buffer;
+    buffer.length = 1;
+    char[] command_buffer;
+    command_buffer.length = 20;
+    //https://dlang.org/library/std/stdio/file.raw_read.html
+    auto data = file.rawRead(buffer);
+    writeln(data);
+    if(data[0] == 1)
     {
         writeln("这是删除文件的机器码");
     }
+    file.rawRead(command_buffer);
+    writeln(command_buffer);
+    //writeln(cast(const(char)[])data);
+    
     file.close();
 }
