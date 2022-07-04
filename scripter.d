48c1ab87ffb8c176c2986b3bc4dbdaa61a93247f
2439905184 <suchenglong018@126.com>
2022-07-04 12:38:00 +0800

    ✨ feat: 实现了文件读取初步

--- scripter.d
+++ scripter.d
@@ -1,26 +1,39 @@
 import std.stdio;
 import std.file;
-//在这里解析二进制指令文件并运行
+//在这里解析二进制指令文件并运行 
+//https://dlang.org/library/std/stdio/file.raw_read.html
+byte[2] read_head(File p_file)
+{
+    byte[2] head;
+    p_file.rawRead(head);
+    return head;
+}
+char[20] read_name(File p_file)
+{
+    char[20] file_name;
+    p_file.rawRead(file_name);
+    return file_name;
+}
 void main(string[] args)
 {
+    writeln(args[1]);
     string file_name = args[1];
     File file = File(args[1],"r");
-    //writeln(args[1]);
-    //先读取两个字节 这里的编译器没做好
-    byte[] buffer;
-    buffer.length = 1;
-    char[] command_buffer;
-    command_buffer.length = 20;
-    //https://dlang.org/library/std/stdio/file.raw_read.html
-    auto data = file.rawRead(buffer);
-    writeln(data);
-    if(data[0] == 1)
-    {
-        writeln("这是删除文件的机器码");
-    }
-    file.rawRead(command_buffer);
-    writeln(command_buffer);
-    //writeln(cast(const(char)[])data);
     
+    byte[] head = read_head(file);
+    byte[1] space;
+    file.rawRead(space);
+    char[] name = read_name(file);
+    writeln("指令码>>>",head,"参数1>>>",name);    
+    //24 is a chunk size
+    /*for(int i=0; i < file.size(); i+=24)
+    {
+        byte[] head = read_head(file);
+        byte[1] space;
+        file.rawRead(space);
+        char[] name = read_name(file);
+        writeln("指令码>>>",head,"参数1>>>",name);    
+    }
+    */
     file.close();
 }
