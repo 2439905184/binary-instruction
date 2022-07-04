ff4365955f242102f4d86f1cb6f574b2d0221917
2439905184 <suchenglong018@126.com>
2022-07-04 16:17:20 +0800

    ✨ feat: 添加了解释器的伪代码

--- scripter.d
+++ scripter.d
@@ -15,6 +15,47 @@ char[20] read_name(File p_file)
     p_file.rawRead(file_name);
     return file_name;
 }
+byte[] create_file = [0,0]; 
+byte[] remove_file = [0,1];
+byte[] read_file = [1,0];
+byte[] write_file = [1,1];
+//解释器
+void MyExecute(byte[] command)
+{
+    byte[] head = command[0..2];
+    byte[] name = command[3..21];
+    byte[] end = command[21..22];//我甚至认为这个;号多余了
+    
+    if(head == create_file)
+    {
+        File f = File(name,"w");
+        //f.create_file()
+        f.close();
+    }
+    if(head == remove_file)
+    {
+        File f = File(name,"w");
+        f.remove();
+        f.close();
+    }
+    if(head == read_file)
+    {
+        File f = File(name,"r");
+        f.readText();
+        f.close();
+    }
+    if(head == write_file)
+    {
+        File f = File(name,"w");
+        f.write();
+        f.close();
+    }
+    else
+    {
+        writeln("错误，不支持的机器码！");
+        writeln(head);
+    }
+}
 void main(string[] args)
 {
     writeln(args[1]);
@@ -22,6 +63,7 @@ void main(string[] args)
     File file = File(args[1],"r");
     
     //24 is a chunk size
+    //todo 在数组合并和切片处理时遇到问题
     for(int i=0; i < file.size(); i+=24)
     {
         byte[] head = read_head(file);
@@ -30,6 +72,8 @@ void main(string[] args)
         char[] name = read_name(file);
         //分号
         file.rawRead(space);
+        //todo
+        //MyExecute();
         writeln("指令码>>>",head,"参数1>>>",name);    
     }
     file.close();
--- test.d
+++ test.d
@@ -1,7 +1,7 @@
 import std.stdio;
 import std.file;
 //使用循环，分段写入文件的示例代码
-void main()
+void Mywrite()
 {
     File f = File("aa.bin","w");
     for(int i = 0; i< 2; i+=1)
@@ -10,4 +10,22 @@ void main()
         f.rawWrite(numbers);
     }
     f.close();
+}
+//类型混合处理
+void byte_array_mix()
+{
+    byte[] a = [0,1];
+    char[] b = ['a','b','c'];
+    byte[] result = a ~ cast(byte[])b;
+    writeln(result);
+    writeln("机器码> ",result[0..2]);
+    byte[] name = result[1..4];
+    writeln("名称> ",cast(char[])name);
+}
+
+void main()
+{
+    //byte_array_mix();
+    byte[] a = [0,1,2,3,4,5,6];
+    writeln(a[2..6]);
 }
